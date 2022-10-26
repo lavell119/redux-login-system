6e7408a7db999066d8ac7e6d51701b4c23348e4d
Lee <leewhiz119@gmail.com>
2022-10-26 19:19:05 -0400

    added input values to login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,32 @@
 import React from 'react'
 
 const Login = () => {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
   return (
     <div className="login">
         <form className="login_form">
             <h1>Login Here</h1>
-            <input type="name" placeholder="Name" />
-            <input type="email" placeholder="Email" />
-            <input type="password" placeholder="Password" />
+            <input 
+                type="name" 
+                placeholder="Name" 
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+            />
+            <input 
+                type="email" 
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)} 
+            />
+            <input 
+                type="password" 
+                placeholder="Password" 
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                />
         </form>
     </div>
   )
